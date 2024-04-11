9ac2e49ddbcddf7f2a41d039518ea52b79a941ef
angel <apineiroandion@danielcastelao.org>
2024-04-11 15:51:41 +0200

    añado metodo que devuelve Arraylist con titulos y id

--- src/FuncionamientoApp/Biblioteca.java
+++ src/FuncionamientoApp/Biblioteca.java
@@ -14,4 +14,11 @@ public class Biblioteca {
     public void setVideos(ArrayList<Video> videos) {
         this.videos = videos;
     }
+    public ArrayList<String> getVideosNames() {
+        ArrayList<String> names = new ArrayList<>();
+        for (int i = 0; i < videos.size(); i++) {
+            names.add("ID: "+i+" Nombre: "+videos.get(i).getTitulo());
+        }
+        return names;
+    }
 }
